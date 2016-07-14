b10ca0b8174a84a7fd6ca413e7d2242fcc89a1f8
Evan <ereller25@gmail.com>
2016-07-14 11:41:02 -0400

    Added arrays, started looping arrays

---
+++ 1.60_arrays.js
@@ -0,0 +1,13 @@
+//Arrays
+
+var majorsArray = ["Poli Sci", "Journalism", "Criminal Justice", "Psych", "Business"];
+
+console.log(majorsArray);
+console.log(majorsArray[3]);
+
+//Can mix values in a JS array
+var randomArray = ["Dogs", 25, "Cats", (22+5)];
+
+console.log(randomArray);
+
+
