95b525893d9b5c437f1fd8e5c8b822c5cb4ad5a2
Evan <ereller25@gmail.com>
2016-07-14 10:09:37 -0400

    Added String

---
+++ 1.10_strings.js
@@ -0,0 +1,18 @@
+//Strings
+//For storing and manipulating text
+
+var firstName = "Evan";
+var lastName = "Reller";
+var sortByLastName = lastName + ", " + firstName;
+
+//Concatenation - putting strings together
+console.log(sortByLastName);
+
+console.log(firstName + " " + lastName + " is a future coder.");
+
+//The list of variables can be anything
+var firstCarMake = "Toyota"
+var firstCarModel = "Camry"
+var year = 1999
+
+console.log ("My first car was a " + year + " " + firstCarMake + " " + firstCarModel);
