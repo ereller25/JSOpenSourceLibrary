9381d47eab50055a402c03d4ea3467049c10416f
Evan <ereller25@gmail.com>
2016-07-14 11:15:53 -0400

    Added while loops

---
+++ 1.50_whileloops.js
@@ -0,0 +1,17 @@
+//While Loops
+var time = 0;
+
+while(time < 10){
+	time++;
+	console.log("Mintues passed: ", time);
+};
+
+var customers = 0;
+while(customers < 100){
+	customers+=10;
+	console.log("People served:", customers);
+}
+if (customers >= 100){
+	console.log("Over 100 served!");
+}
+
