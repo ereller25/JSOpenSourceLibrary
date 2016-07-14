04dec80d75db0c3e7ae004da462154e890963c1e
Evan <ereller25@gmail.com>
2016-07-14 11:46:12 -0400

    Finished looping arrays

---
+++ 1.61_arrays_loops.js
@@ -0,0 +1,15 @@
+//Looping Arrays
+var majorsArray = ["Poli Sci", "Journalism", "Criminal Justice", "Psych", "Business"];
+//to list majors
+for (var i in majorsArray){
+	console.log(majorsArray[i]);
+};
+
+//or as a function to do the same 
+function listMajors(){
+	for(var i=0; i< majorsArray.length; i++){
+		console.log(majorsArray[i]);
+	}
+}
+
+listMajors();
