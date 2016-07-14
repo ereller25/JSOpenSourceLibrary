6048bc5d3c6a88e1a49fc0f963fa3bd2fae7c09b
Evan <ereller25@gmail.com>
2016-07-14 11:26:21 -0400

    Added for loops

---
+++ 1.51_forloops.js
@@ -0,0 +1,17 @@
+//For Loops
+for (var i = 70; i > 1; i-=7){
+	console.log(i)};
+
+for (var i = 0; i<=100; i+10)
+
+
+
+
+for(var i = 0; i <= 100; i+=10){
+	if(i === 80){
+		console.log(i + " so close");
+	}else if(i === 90){
+		console.log(i + " nearly there");
+	}
+	console.log(i);
+};
