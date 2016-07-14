deab477fd4ed5a1d5f93246d6ae49f32ac772ceb
Evan <ereller25@gmail.com>
2016-07-14 11:03:29 -0400

    Added functions

--- 1.40_functions.js
+++ 1.40_functions.js
@@ -1,2 +1,25 @@
 //Functions
 
+function addNumbers(){
+	var x = 5
+	var y = 10
+	console.log(x + y);
+};
+
+addNumbers();
+
+function multiplyNumbers(z,q){
+	console.log(z * q);
+};
+
+multiplyNumbers(5, 10);
+
+function shootingPercentage(hits, misses){
+	console.log(hits / misses);
+}
+shootingPercentage(7, 11)
+
+function effectiveFieldGoals(fgm, tpm, fga){
+	console.log((fgm + 0.5 * tpm)/ fga);
+}
+effectiveFieldGoals(5, 2, 10);
