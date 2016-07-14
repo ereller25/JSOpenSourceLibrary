004ca2ab4927fbe7506e3e0785687da7cc23b2c2
Evan <ereller25@gmail.com>
2016-07-14 11:58:02 -0400

    Objects properties completed

---
+++ 1.70_objects_properties.js
@@ -0,0 +1,11 @@
+//Objects Properties
+var firstCar = {
+	make : "Toyota",
+	model : "Camry",
+	yearBought : 1999,
+	yearSold : 2007,
+	miles : 120000
+};
+
+console.log(firstCar.make);
+console.log(firstCar);
