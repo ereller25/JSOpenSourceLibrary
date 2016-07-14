39a033e439eb61f46ee0cf0da36385a354220998
Evan <ereller25@gmail.com>
2016-07-14 09:56:39 -0400

    Integers finished

--- 1.00_integers.js
+++ 1.00_integers.js
@@ -1,2 +1,23 @@
 console.log("Hello World");
 
+//Integers
+var x = 1
+console.log(x);
+
+//Simple math with Integers
+x = 5
+var y =10
+console.log(x + y);
+
+//Variable can be anything
+var yearsWorking = 6
+var collegeGrad = 2007
+
+//Operators - simple math symbols
+5 + 5;
+5 - 5;
+5 * 5;
+5 / 5;
+5 % 5; //to return a remainder
+
+
