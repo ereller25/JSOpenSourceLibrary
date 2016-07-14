822e7d7c939f223cad66619d9599daad4f530cc9
Evan <ereller25@gmail.com>
2016-07-14 13:52:19 -0400

    Objects inheritance

---
+++ 1.72_objects_inheritance.js
@@ -0,0 +1,35 @@
+//Objects Inheritance
+var car = {	
+	make 			: "",
+	model 			: "",
+	yearBought 		: 0,
+	yearsOwned 		: 0,
+	miles 			: 0,
+	maintenanceCost	: 0,
+	carInfo			: function(){
+		console.log("My first car was a " + this.make + " " + this.model)
+	},
+	favCar			: function(){
+		console.log();
+		if (this.yearsOwned >= 5 && this.maintenanceCost < 5000){
+			console.log(this.model + " was my best car.")
+		}else{
+			console.log(this.model + " got the job done, kind of.")
+		}
+	}
+}
+
+var tCamry = Object.create(car); 
+	tCamry.make = "Toyota";
+	tCamry.model = "Camry";
+	tCamry.yearBought = 1999;
+	tCamry.yearsOwned = 4;
+	tCamry.miles = 140000;
+	tCamry.maintenanceCost = 4;
+
+tCamry.favCar();
+
+
+
+
+
