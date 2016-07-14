5b3b352a075614db5e92532128bb5306d978660a
Evan <ereller25@gmail.com>
2016-07-14 10:36:04 -0400

    Added Booleans

---
+++ 1.20_booleans.js
@@ -0,0 +1,31 @@
+//Booleans - true/false statements
+
+var anxious = true;
+var learning = true;
+var mastery = false;
+
+console.log(mastery);
+
+//Operators
+//Will use these:
+// > greater than
+// < less than
+// == equal to
+// >= greater than or equal to
+// <= less than or equal to
+// && and
+// || or
+// ! not
+
+var math = 10 > 5;
+console.log(math);
+//Can be combined with a string
+console.log("Is ten bigger than five? " + math);
+
+//Other operators to compare two things:
+// && and
+// || or
+// ! not
+console.log(math && 2>1);
+console.log(10 != 5);
+
